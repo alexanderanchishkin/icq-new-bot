3eda4b3dfcb944043ea5bfda7a7fa284a2c0398f
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-05-03 11:13:50 +0300

    Initial commit

---
+++ core/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gettingstarted project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from core.app import app
+
+os.environ.setdefault("FLASK_APP", "core.app")
+
+app.run()
