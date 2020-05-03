f4035e4e9d96a73a7ae68d0744550fd3db625da8
nikittos5 <fursovnikita@yandex.ru>
2020-05-03 15:50:39 +0300

    Can Add/Edit records in Users, Advices, State with utilities\DBExplorer

--- utilities/db_explorer.py
+++ utilities/db_explorer.py
@@ -1,5 +1,7 @@
 from peewee import *
 from models.users import Users
+from models.state import State
+from models.advices import Advices
 
 class DBExplorer:
     def __enter__(self):
@@ -13,12 +15,32 @@ class DBExplorer:
                                 password='5b444a910f1acd6eedb48fd391bcb5c891e53eba17ee1401a513aba0e783e12e',
                                 host='ec2-3-211-48-92.compute-1.amazonaws.com', port=5432)
 
-    def write(self, args):
+    def write_users(self, args):
         self.db.connect()
         user = Users.insert(**args)
         user.execute()
 
-    def update(self, args, search_id=1):
+    def update_users(self, args, search_id=1):
         self.db.connect()
-        user = Users.update(**args).where(Users.icq_id == search_id)
+        user = Users.update(**args).where(Users.user_id == search_id)
         user.execute()
+
+    def write_advices(self, args):
+        self.db.connect()
+        advices = Advices.insert(**args)
+        Advices.execute()
+
+    def update_advices(self, args, search_id=1):
+        self.db.connect()
+        advices = Advices.update(**args).where(Advices.user_id == search_id)
+        advices.execute()
+
+    def write_states(self, args):
+        self.db.connect()
+        states = State.insert(**args)
+        states.execute()
+
+    def update_states(self, args, search_id=1):
+        self.db.connect()
+        states = State.update(**args).where(State.user_id == search_id)
+        states.execute()
