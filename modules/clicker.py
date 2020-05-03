70cce67407e2fd7b6f4dab0b5382a03a96fc1b2f
nikittos5 <fursovnikita@yandex.ru>
2020-05-03 20:36:04 +0300

    User + Clicker

--- modules/clicker.py
+++ modules/clicker.py
@@ -1,30 +1,25 @@
 from peewee import *
+import datetime
 
 class BaseModel(Model):
-
     class Meta:
         database = PostgresqlDatabase('dc9gn4kbsdd0mi', user='onpzldzoogstwe',
                                 password='5b444a910f1acd6eedb48fd391bcb5c891e53eba17ee1401a513aba0e783e12e',
                                 host='ec2-3-211-48-92.compute-1.amazonaws.com', port=5432)
 
-class Users(BaseModel):
-    id = PrimaryKeyField(null=False)
-    username = CharField(max_length=250, default='')
+class Clicker(BaseModel):
+    id = PrimaryKeyField()
+    title = CharField(max_length=250, default='')
+    text = TextField(default='')
     name = CharField(max_length=250, default='')
-    user_id = CharField(max_length=250, default = '')
+    created_at = DateTimeField(default=datetime.datetime.now())
 
     class Meta:
-        db_table = "users"
-
+        db_table = "clicker"
 
 if __name__ == '__main__':
     db = PostgresqlDatabase('dc9gn4kbsdd0mi', user='onpzldzoogstwe',
                              password='5b444a910f1acd6eedb48fd391bcb5c891e53eba17ee1401a513aba0e783e12e',
                              host='ec2-3-211-48-92.compute-1.amazonaws.com', port=5432)
     db.connect()
-    Users.create_table(safe=True)
-    # db = PostgresqlDatabase('dc9gn4kbsdd0mi', user='onpzldzoogstwe',
-    #                          password='5b444a910f1acd6eedb48fd391bcb5c891e53eba17ee1401a513aba0e783e12e',
-    #                          host='ec2-3-211-48-92.compute-1.amazonaws.com', port=5432)
-    # db.connect()
-    # print(db)
+    Clicker.create_table(safe=True)
