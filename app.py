3c35c35cc8157035d2da460a20d7ddf9c2401d87
GentleGame <banderes94@gmail.com>
2020-05-04 16:16:14 +0300

    limpopo

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def message_cb(bot, event):
     if event.text=="/random":
         bot.send_text(chat_id=event.from_chat, text=str(randrange(101)))
     elif event.text=="/start":
-        bot.send_text(chat_id=event.from_chat, text="Registration in process")
+        bot.send_text(chat_id=event.from_chat, text="Будьте готовы сражаться! Вирус на подходе!")
         try:
             explorer.write_user({'username':event.data['from'].get('nick', ''),
                                 'name': event.data['from'].get('firstName', '')+' '+event.data['from'].get('lastName', ''),
@@ -68,7 +68,7 @@ def message_cb(bot, event):
     elif event.text == "/create_COVID":
         users = explorer.get_user_ids()
         explorer.create_monster({"hp":50000000, "endbattle": int(time.time())+12*60*60})
-        text = "На карте Черноруссии появился новый вирус! \nУ него зафиксировано {0} HP. Поспеши уничтожить его! \n\n >> /time_to_kill <<".format(explorer.attack_monster(damage=0, user_id=event.from_chat))
+        text = "На карте Лимпопо появился новый вирус! \nУ него зафиксировано {0} HP. Поспеши уничтожить его! \n\n >> /time_to_kill <<".format(explorer.attack_monster(damage=0, user_id=event.from_chat))
         send_alerts(users, text)
     elif event.text == "/time_to_kill":
         kill_id = explorer.get_kill_id(user_id=event.from_chat)
